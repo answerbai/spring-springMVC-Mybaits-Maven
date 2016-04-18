b152559864735ada0d055c5165e39eb490d7e05a
dy <2711099209@qq.com>
2016-04-18 09:40:22 +0800

    spring-springMVC-Mybaits-Maven项目搭建

---
+++ src/main/java/com/controller/ServletController.java
@@ -0,0 +1,127 @@
+package com.controller;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.model.Student;
+import com.service.ServiceOffer;
+
+@Controller
+public class ServletController {
+	private Logger logger = Logger.getLogger(ServletController.class);
+	@Resource
+	private ServiceOffer serviceOffer;
+
+	@RequestMapping("/queryOne")
+	public String queryOne(HttpServletRequest request, Model model) {
+		if (null == request.getParameter("id") || "".equals(request.getParameter("id"))) {
+			String message = "未传入查询的ID号";
+			logger.info(message);
+			model.addAttribute("message", message);
+			return "error";
+		}
+		int id = Integer.valueOf(request.getParameter("id"));
+		Student s = serviceOffer.queryOne(id);
+		if (s != null) {
+			model.addAttribute("student", s);
+			return "showStudentOne";
+		} else {
+			String message = "数据库中没有编号为‘" + id + "'的同学的信息！";
+			logger.info(message);
+			model.addAttribute("message", message);
+			return "error";
+		}
+	}
+
+	@RequestMapping("/queryList")
+	public String queryList(HttpServletRequest request, Model model) {
+		List<Student> s = serviceOffer.queryList();
+		if (s.size() > 0) {
+			model.addAttribute("student", s);
+			return "showStudentList";
+		} else {
+			String message = "数据库中没有同学的信息！";
+			logger.info(message);
+			model.addAttribute("message", message);
+			return "error";
+		}
+	}
+
+	@SuppressWarnings("unused")
+	@RequestMapping("/insetInfo")
+	public String insetInfo(HttpServletRequest request, Model model) {
+		Student s = new Student();
+		String name = request.getParameter("name");
+		if (null == name || "".equals(name)) {
+			String message = "未传入姓名";
+			logger.info(message);
+			model.addAttribute("message", message);
+			return "error";
+		}
+		s.setName(name);
+		String temp = "^\\d+";
+		String age = request.getParameter("age");
+		if (null == age || "".equals(age)) {
+			String message = "未传入年龄";
+			logger.info(message);
+			model.addAttribute("message", message);
+			return "error";
+		}
+		if (age.matches(temp)) {
+			s.setAge(Integer.valueOf(request.getParameter("age")));
+		} else {
+			String message = "传入的年龄不是纯数字！传入的值=" + request.getParameter("age");
+			logger.info(message);
+			model.addAttribute("message", message);
+			return "error";
+		}
+		try {
+			serviceOffer.insertInfo(s);
+		} catch (Exception e) {
+			String message = "插入数据出错！";
+			logger.info(message);
+			model.addAttribute("message", message);
+			return "error";
+		}
+		Student student = serviceOffer.queryOne(serviceOffer.queryId());
+		if (s != null) {
+			model.addAttribute("student", student);
+			return "showStudentOne";
+		} else {
+			String message = "数据库中没有编号为‘" + serviceOffer.queryId() + "'的同学的信息！";
+			logger.info(message);
+			model.addAttribute("message", message);
+			return "error";
+		}
+	}
+
+	@RequestMapping("/requestCookie")
+	public String requestCookie(HttpServletRequest request, Model model, HttpServletResponse respones) throws UnsupportedEncodingException {
+		Cookie c = new Cookie("dy", URLEncoder.encode("丁毅", "utf-8"));
+		respones.addCookie(c);
+		return "redirect:/responesCookie";
+	}
+
+	@RequestMapping("/responesCookie")
+	public String responesCookie(HttpServletRequest request, Model model, HttpServletResponse respones) throws UnsupportedEncodingException {
+		Cookie[] c = request.getCookies();
+		for (Cookie cc : c) {
+			if ("dy".equals(cc.getName())) {
+				System.out.println("CookieDomain="+cc.getDomain() + ",Cookie存放的数据是：(key=" + cc.getName() + ",value=" +URLDecoder.decode(cc.getValue(),"utf-8") + ")");
+			}//end--if 
+		}//end--for
+		return "error";
+	}
+}
---
+++ src/main/java/com/service/ServiceOffer.java
@@ -0,0 +1,31 @@
+package com.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.dao.StudentDao;
+import com.model.Student;
+
+@Component("serviceOffer")
+public class ServiceOffer {
+	//@Autowired
+	private StudentDao studentDao;
+
+	public Student queryOne(int id) {
+		return studentDao.queryOneById(id);
+	}
+	public Student queryOne(String name) {
+		return studentDao.queryOneByName(name);
+	}
+	public List<Student> queryList() {
+		return studentDao.queryList();
+	}
+	public void insertInfo(Student s){
+		studentDao.insertInfo(s);
+	}
+	public int queryId(){
+		return studentDao.queryId();
+	}
+}
