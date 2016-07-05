4e2f0a0ff4abd58a5737645d9bfc9691eb655fb6
dy <2711099209@qq.com>
2016-07-05 18:07:40 +0800

    增加sql文件

---
+++ db.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `student` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `age` int(10) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
